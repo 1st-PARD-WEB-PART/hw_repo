ee5e87d62c428c2b57b6cecbdfaee04cba0f47c9
KSH-KOR <54264875+KSH-KOR@users.noreply.github.com>
2023-03-14 10:19:20 +0900

    hw1 done - info modified

--- week1/hw1/src/App.js
+++ week1/hw1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          21900136 김신후, PARD 화이팅~~
         </p>
         <a
           className="App-link"
@@ -21,5 +21,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
